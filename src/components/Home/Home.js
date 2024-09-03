b2d36c8a869a09f51154cc38bf9d9c5a49aa629a
Rutvij Bhesaniya <rutvij.bhesaniya@yudizsolution.com>
2024-09-03 16:26:40 +0530

    DevOps team name will be show on website page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Shaikh Aaqib</strong>
+                <strong className="main-name"> DevOps Team</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
