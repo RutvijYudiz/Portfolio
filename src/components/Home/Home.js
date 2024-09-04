30b68402f74fbfbe34708b74c9254c7fbf0f766c
Rutvij Bhesaniya <rutvij.bhesaniya@yudizsolution.com>
2024-09-04 10:15:58 +0530

    changes of text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> DevOps12 Team</strong>
+                <strong className="main-name"> DevOps12345 Team</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
