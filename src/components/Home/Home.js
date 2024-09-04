f3b9be0d44ab1db69a1cfd1d753a17a661e79e2d
Rutvij Bhesaniya <rutvij.bhesaniya@yudizsolution.com>
2024-09-04 13:25:52 +0530

    changes in home.js and yamlfile for systemd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Devops12345 Team</strong>
+                <strong className="main-name"> Devops9090 Team</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
